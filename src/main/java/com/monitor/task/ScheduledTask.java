f10a6d82b2daaff07d2d3c893f6549b0fe0cc9a8
stephenyi <stephenyi@tencent.com>
2020-06-15 16:50:14 +0800

    @Async注解含义

--- src/main/java/com/monitor/task/ScheduledTask.java
+++ src/main/java/com/monitor/task/ScheduledTask.java
@@ -20,7 +20,7 @@ public class ScheduledTask {
 	//基于注解@Scheduled默认为单线程，开启多个任务时，任务的执行时机会受上一个任务执行时间的影响
 	//添加Async改为多线程
 
-	@Async(value = "pushThreadPool")
+	@Async(value = "pushThreadPool")//不设置value值时默认每次开启线程SimpleAsyncTaskExecutor-175
 //	@Scheduled(fixedRate = 5000)	//注释该注解表示关闭任务
 	@Scheduled(cron = "0/5 * * * * ?")
 	public void reportCurrent(){
